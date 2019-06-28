ae5805111053fbb34739e875ee410c9cc0d3df92
jalsingh38 <jals@lucidux.com>
2019-06-28 15:24:08 +0530

    test

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -37,7 +37,7 @@
                 <form action="{{ route('posts.destroy', $post->id)}}" method="post">
                   @csrf
                   @method('DELETE')
-                  <button class="btn btn-danger" type="submit">Delete</button>
+                  <button  class="btn btn-danger" type="submit">Delete</button>
                 </form>
             </td>
         </tr>
